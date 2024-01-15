f6cbe71861ae6ffd5ad5703003460d91ea83d31b
shivarhombuz <shiva@rhombuz.io>
2024-01-15 21:25:35 +0530

    fix hiding the div

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import Navbar from './components/Navbar/navbar';
 import MainConatainer from './components/MainBody/MainContainer';
 import * as Styles from "./components/MainBody/style";
 
 function App() {
   const trackedDivRef = useRef();
-    const [initialTop, setInitialTop] = useState(0);
-    const [showNavDiv, setShowNavDiv] = useState(false);
+  const [initialTop, setInitialTop] = useState(0);
+  const [showNavDiv, setShowNavDiv] = useState(false);
 
-    const handleScroll = () => {
-        const rect = trackedDivRef.current.getBoundingClientRect();
-        const traveledDistance = initialTop - rect.top;
-        if(traveledDistance >= 0){
-          setShowNavDiv(true);
-        }
-        else {
-          setShowNavDiv(false);
-        }
+  const handleScroll = useCallback(() => {
+    const rect = trackedDivRef.current.getBoundingClientRect();
+    const traveledDistance = initialTop - rect.top;
+    // console.log('traveledDistance', traveledDistance);
+    if (traveledDistance >= 80) {
+      setShowNavDiv(true);
+    }
+    else {
+      setShowNavDiv(false);
+    }
+  }, [initialTop]);
+
+  useEffect(() => {
+    // Set initial top position when the component mounts
+    setInitialTop(trackedDivRef.current.getBoundingClientRect().top);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-
-    useEffect(() => {
-        // Set initial top position when the component mounts
-        setInitialTop(trackedDivRef.current.getBoundingClientRect().top);
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+  }, [handleScroll]);
 
   return (
     <Styles.FullPage>
-      <Navbar showNavDiv={showNavDiv}/>
+      <Navbar showNavDiv={showNavDiv} />
       <MainConatainer trackRef={trackedDivRef} />
     </Styles.FullPage>
   );
